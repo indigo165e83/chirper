7e83e2b9c0b50f8b4782e448d182342d44c4461f
indigo165e83 <151335613+indigo165e83@users.noreply.github.com>
2026-09-13 10:26:43 +0900

    feat: Memo の削除と一覧の操作ボタンを追加 (#6)

--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -8,13 +8,7 @@
         <a href="{{ route('memos.create') }}" class="bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create Memo</a>
     </div>
     @forelse ($memos as $memo)
-        <div class="card w-full bg-base-100 shadow-xl mb-4">
-            <div class="card-body">
-                <h2 class="card-title">{{ $memo->title }}</h2>
-                <p>{{ $memo->body }}</p>
-                <p>{{ $memo->is_draft ? 'Draft' : 'Published' }}</p>
-            </div>
-        </div>
+        <x-memo :memo="$memo" />
     @empty
         <p>No memos found.</p>
     @endforelse
